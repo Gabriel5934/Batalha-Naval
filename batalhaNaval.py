8c71858a815d7babca890574108bb2bcd99cde69
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2019-11-02 23:15:56 -0300

    Antes de iniciar geração do tabuleiro do bot

--- batalhaNaval.py
+++ batalhaNaval.py
@@ -3,22 +3,26 @@
 import numpy as np
 import time
 import sys
+from random import randrange
 
 # Definindo variáveis e funções
 playerBoard = (np.zeros((8, 8), dtype=str))
 playerBoard.fill("■")
 
-arsenal = ["porta aviões", "navio de escolta", "navio canhoneiro", "torpedeiro"]
-setUp = 0
+arsenal = {"porta aviões":"۞", "navio de escolta":"♨", "navio canhoneiro":"☠", "torpedeiro":"☢"}
+changeBoat = 0
 
 def printBoard():
-    print(f"{playerBoard}\n\n\n\n\n")
+    for i in range(0, 8):
+        print(f"\t\t\t\t\t\t\t\t{playerBoard[i]}")
+    print("\n")
 
 def tryAgain():
     print(chr(27) + "[2J")
-    print("Algo deu errado!")
+    print("\t\t\t\t\t\t\t\tAlgo deu errado!")
     time.sleep(1)
-    print("Tente Novamente!") 
+    print("\t\t\t\t\t\t\t\tTente Novamente!\n")
+    printBoard() 
 
 # Tela inicial
 print('''
@@ -40,44 +44,44 @@ print(chr(27) + "[2J")
 printBoard()
 
 # Iniciando o jogo
+keysList = list(arsenal)
 for i in range(0, 4):
-    boat = arsenal[setUp]
+    try:
+        boat = (keysList[changeBoat])
+    except:
+        print("Isso não deveria acontecer")
 
     # Definindo posição da embarcação
     validation = False
     while validation == False:
-        row = int(input(f"Digite a coluna onde seu {boat} começa: "))
-        column = int(input(f"Digite a linha onde seu {boat} começa: "))
-        orientation = int(input(f"Para deixar seu {boat} em pé, digite '1', para deixá-lo deitado, digite '2': "))
+        row = int(input(f"\t\t\t\t\t\t\t\tDigite a linha onde ficará a proa do seu {boat}: "))
+        column = int(input(f"\t\t\t\t\t\t\t\tDigite a coluna onde ficará a proa do seu {boat}: "))
+        orientation = int(input(f"\t\t\t\t\t\t\t\tPara deixar seu {boat} em pé, digite '1', para deixá-lo deitado, digite '2': "))
 
         # Validando a posição inserida pelo user
         if row > 8 or column > 8:
             tryAgain()
         elif orientation != 1 and orientation != 2:
             tryAgain()
-        elif playerBoard[row - 1][column - 1] == "☢":
+        elif playerBoard[row - 1][column - 1] == arsenal[boat]:
             tryAgain()
-        if orientation == 1:
-            if (column - 1) + 4 > 8:
-                tryAgain()
-        if orientation == 2:
-            if (row - 1) + 4 > 8:
-                tryAgain()
+        elif orientation == 1 and (column - 1) + 4 > 8:
+            tryAgain()
+        elif orientation == 2 and (row - 1) + 4 > 8:
+            tryAgain()
         else:
             validation = True
 
     # Colocando as embarcações no tabuleiro
     if orientation == 1:
         for i in range(0, 4):
-            playerBoard[(row - 1) + i][column - 1] = "☢"
+            playerBoard[(row - 1) + i][column - 1] = arsenal[boat]
     if orientation == 2:
         for i in range(0, 4):
-            playerBoard[row - 1][(column - 1) + i] = "☢"
-    setUp += 1
+            playerBoard[row - 1][(column - 1) + i] = arsenal[boat]
+    changeBoat += 1
 
     printBoard()
-
-
 
 print("Gerando tabuleiro do oponete")
 for i in range(0, 10):
@@ -85,6 +89,8 @@ for i in range(0, 10):
     sys.stdout.flush()
     time.sleep(0.5)
 print("\nGeração concluída!")
+
+
 
 
 
