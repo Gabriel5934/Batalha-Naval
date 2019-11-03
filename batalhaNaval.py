4579c447666e6efa713c809232e54fdb6403fd86
Gabriel Andrade <gabriel.andrade@outlook.com.br>
2019-11-03 08:18:16 -0300

    Geração do tabuleiro do oponente concluída

--- batalhaNaval.py
+++ batalhaNaval.py
@@ -20,7 +20,7 @@ def printBoard():
 def tryAgain():
     print(chr(27) + "[2J")
     print("\t\t\t\t\t\t\t\tAlgo deu errado!")
-    time.sleep(1)
+    time.sleep(0.5)
     print("\t\t\t\t\t\t\t\tTente Novamente!\n")
     printBoard() 
 
@@ -57,31 +57,99 @@ for i in range(0, 4):
         row = int(input(f"\t\t\t\t\t\t\t\tDigite a linha onde ficará a proa do seu {boat}: "))
         column = int(input(f"\t\t\t\t\t\t\t\tDigite a coluna onde ficará a proa do seu {boat}: "))
         orientation = int(input(f"\t\t\t\t\t\t\t\tPara deixar seu {boat} em pé, digite '1', para deixá-lo deitado, digite '2': "))
+        if changeBoat == 0:
+            occupiedList = []
 
         # Validando a posição inserida pelo user
         if row > 8 or column > 8:
             tryAgain()
         elif orientation != 1 and orientation != 2:
             tryAgain()
-        elif playerBoard[row - 1][column - 1] == arsenal[boat]:
+        elif orientation == 1 and (row - 1) + 4 > 8:
             tryAgain()
-        elif orientation == 1 and (column - 1) + 4 > 8:
+        elif orientation == 2 and (column - 1) + 4 > 8:
             tryAgain()
-        elif orientation == 2 and (row - 1) + 4 > 8:
-            tryAgain()
-        else:
-            validation = True
+        elif orientation == 1:
+            for i in range(0, 4):
+                if [row + i, column] in occupiedList:
+                    tryAgain()
+                else:
+                    validation = True
+        elif orientation == 2:
+            for i in range(0, 4):
+                if [row, column + i] in occupiedList:
+                    tryAgain()
+                else:
+                    validation = True
 
     # Colocando as embarcações no tabuleiro
     if orientation == 1:
         for i in range(0, 4):
             playerBoard[(row - 1) + i][column - 1] = arsenal[boat]
+            occupiedList.append([row + i, column])
     if orientation == 2:
         for i in range(0, 4):
             playerBoard[row - 1][(column - 1) + i] = arsenal[boat]
+            occupiedList.append([row, column + i])
     changeBoat += 1
 
     printBoard()
+
+# Gerando tabuleio do computador
+botBoard = (np.zeros((8, 8), dtype=str))
+botBoard.fill("■")
+changeBoat = 0
+
+keysList = list(arsenal)
+for i in range(0, 4):
+    try:
+        boat = (keysList[changeBoat])
+    except:
+        print("Isso não deveria acontecer")
+
+    # Definindo posição da embarcação
+    validation = False
+    while validation == False:
+        row = randrange(9)
+        column = randrange(9)
+        orientation = randrange(3)
+        if changeBoat == 0:
+            occupiedList = []
+
+        # Validando a posição inserida pelo user
+        if row > 8 or column > 8:
+            validation = False
+        elif orientation != 1 and orientation != 2:
+            validation = False
+        elif orientation == 1 and (row - 1) + 4 > 8:
+            validation = False
+        elif orientation == 2 and (column - 1) + 4 > 8:
+            validation = False
+        elif row == 0 or column == 0 or orientation == 0:
+            validation = False
+        elif orientation == 1:
+            for i in range(0, 4):
+                if [row + i, column] in occupiedList:
+                    validation = False
+                else:
+                    validation = True
+        elif orientation == 2:
+            for i in range(0, 4):
+                if [row, column + i] in occupiedList:
+                    validation = False
+                else:
+                    validation = True
+
+    # Colocando as embarcações no tabuleiro
+    if orientation == 1:
+        for i in range(0, 4):
+            botBoard[(row - 1) + i][column - 1] = arsenal[boat]
+            occupiedList.append([row + i, column])
+    if orientation == 2:
+        for i in range(0, 4):
+            botBoard[row - 1][(column - 1) + i] = arsenal[boat]
+            occupiedList.append([row, column + i])
+    changeBoat += 1
 
 print("Gerando tabuleiro do oponete")
 for i in range(0, 10):
@@ -90,6 +158,7 @@ for i in range(0, 10):
     time.sleep(0.5)
 print("\nGeração concluída!")
 
+print(botBoard)
 
 
 
